4ad7d2270a9ea1facad6b80fa8b004c6016efdf2
AAHANjainn <92387845+AAHANjainn@users.noreply.github.com>
2023-04-11 18:54:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function room(){
+    window.location="room.html"
+}
+
+function hall(){
+    window.location="hall.html"
+}
+
+function kitchen(){
+    window.location="kitchen.html"
+}
+
+
+function bathroom(){
+    window.location="bathroom.html"
+}
+
+// function preload()
+// {
+  // bathroom_pic="bathroom.jpg";
+ //  room_pic="room.jpg";
+  // hall_pic = "hall.jpg";
+ //  kitchen_pic = "kitchen.jpg";  
+ //}
---
+++ room.js
@@ -0,0 +1,59 @@
+function back(){
+    window.location= "index.html"; 
+ }
+
+model__status = "";
+object = [];
+
+function preload()
+ {
+    room_pic = loadImage("room.jpg");
+ }
+
+ function setup(){
+    canvas = createCanvas(500,400);
+    canvas.center();
+    objectDetector = ml5.objectDetector("cocossd",modelLoaded);
+//document.getElementById("status").innerHTML = " detecting objects"
+    // video = createCapture(VIDEO);
+ }
+
+ function modelLoaded(){
+    console.log("model is Loaded");
+    model__status = true;
+    objectDetector.detect(room_pic,gotResults);
+}
+
+function gotResults(error,results)
+{
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        object = results;
+    }
+    
+}
+
+
+
+ function draw(){
+    image(room_pic,0,0,500,400);
+
+    if(model__status != ""){
+        for(var i = 0;i<object.length;i++){
+            noFill();
+            stroke("red");
+            strokeWeight(2);
+            textSize(20);
+    rect(object[i].x,object[i].y,object[i].width,object[i].height)
+    fill("white");
+    text(object[i].label,object[i].x,object[i].y-3);
+    percent =round(object[i].confidence*100)+"%"; 
+    width = object[i].width;
+    text(percent,object[i].x+width,object[i].y-3);
+   // document.getElementById("status").innerHTML = "  Objects detected"
+        }
+    }
+ }
